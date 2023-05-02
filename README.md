7ac4b8cd9ef5983653da7e25db819189c25079b9
DarkStars0 <ahmadjehadalanati1@gmail.com>
2023-05-02 17:18:24 +0000

    task done

---
+++ README.md
@@ -0,0 +1,9 @@
+# LAB - Class 01
+
+## Project: snakes_cafe
+
+### Author: Ahmad Jehad AlAnati
+
+#### How to initialize/run your application:
+
+just run this line in your terminal "python main.py"
--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,3 +1,5 @@
+import re
+
 menu = {
     "Appetizers":["Wings","Cookies","Spring Rolls"],
     "Entrees":["Salmon","Steak","Meat Tornado","A Literal Garden"],
@@ -5,12 +7,15 @@ menu = {
     "Drinks":["Coffee","Tea","Unicorn Tears"]
 }
 
+user_orders ={}
+
 def intro():
     str_intro = '''**************************************
 **    Welcome to the Snakes Cafe!   **
 **    Please see our menu below.    **
 **
 ** To quit at any time, type "quit" **
+** To see your cart, type "cart" **
 **************************************
 '''
     print(str_intro)
@@ -25,10 +30,50 @@ def showmenu():
 
 def user_input_menu():
     user_input = input(">")
+    user_input = user_input.replace(user_input[0],user_input[0].upper())
     return user_input
+
+def orders():
+    show_promt = '''
+***********************************
+** What would you like to order? **
+***********************************'''
+    print(show_promt)
+    order = user_input_menu()
+    while(order.lower() != "quit"):
+        if(order.lower() == "cart"):
+            show_orders()
+            order = user_input_menu()
+            continue
+        if check_if_exists(order):
+            if order in user_orders:
+                user_orders[order] = user_orders[order]+1
+            else:
+                user_orders[order] = 1
+        else:
+            print("sorry this item dosen't exists in our menu")
+        order = user_input_menu()
+
+def check_if_exists(order):
+    if order in menu["Appetizers"]:
+        return True
+    if order in menu["Desserts"]:
+        return True
+    if order in menu["Drinks"]:
+        return True
+    if order in menu["Entrees"]:
+        return True
+    return False
+
+def show_orders():
+    for item in user_orders:
+        str = "{} order of {} has been added to your meal"
+        print(str.format(user_orders[item],item))
+
 
 def main():
     intro()
     showmenu()
-    
+    orders()
+
 main()
